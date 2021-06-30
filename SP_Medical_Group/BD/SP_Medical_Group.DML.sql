d4374825b3cbbb07e7dc26901a899e8082a358e2
AndreBarroso495 <andregamer129@gmail.com>
2021-06-30 16:25:24 -0300

    Projeto COncluido

---
+++ SP_Medical_Group/BD/SP_Medical_Group.DML.sql
@@ -0,0 +1,21 @@
+USE SP_Medical_Group;
+
+SELECT * FROM usuario;
+GO
+
+SELECT * FROM paciente;
+GO
+
+SELECT * FROM medico;
+GO
+
+SELECT * FROM consulta;
+GO
+
+SELECT * FROM especialidade;
+GO
+
+SELECT * FROM tipoUsuario;
+GO
+
+SELECT * FROM clinica;
